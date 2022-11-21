28227364d1f44bfa7bf13d79a552dd08231cafc6
Masatoshi SEKI <m_seki@mva.biglobe.ne.jp>
2022-11-22 00:05:45 +0900

    hatena

--- city/bar.rb
+++ city/bar.rb
@@ -1,51 +1,38 @@
 require 'json'
 
-class ForPie
-  def initialize(report)
-    @report = report
-  end
-
-  def to_chart_data
-    total = @report['deck_count']
-    data = []
-    label = []
-    @report['cluster'].each do |c|
-      total -= c[0]
-      data << c[0]
-      label << [c[1], c[2]].join(' ')
-    end
-    label << 'other'
-    data << total
-    {
-      'labels' => label ,
-      'datasets' => [{
-        'data' => data
-      }]
-    }.to_json
-  end
-end
-
 class ForBar
   Paired12 = ['#a6cee3', '#1f78b4', '#b2df8a', '#33a02c', '#fb9a99', '#e31a1c', '#fdbf6f', '#ff7f00', '#cab2d6', '#6a3d9a', '#ffff99', '#b15928']
 
   def initialize(ary)
     @ary = ary
+    make_dataset
   end
+  attr_reader :deck
 
   def to_chart_data
+    {
+      "labels" => @labels,
+      "datasets" => @dataset
+    }
+  end
+
+  def make_dataset
     dict = Hash.new {|h,k| h[k] = [0] * @ary.size}
+    deck = Hash.new {|h,k| h[k] = Array.new(@ary.size)}
     other = []
     labels = []
     @ary.each_with_index do |report, i|
       labels << (report['range'].split('.').first)
       total = report['deck_count']
-      report['cluster'][0,6].each do |c|
+      report['cluster'][0,8].each do |c|
         dict[c[2]][i] = c[0]
+        deck[c[2]][i] = c[1]
         total -= c[0]
       end
       other << total
     end
     dict['other'] = other
+    deck['other'] = Array.new(@ary.size)
     color = Paired12.dup
     dataset = dict.map do |k, v|
       color.rotate!
@@ -53,10 +40,9 @@ class ForBar
       {"label" => k, "data" => v, "stack" => "stack-1", "backgroundColor" => c + "ee", "borderColor" => c}
     end
 
-    {
-      "labels" => labels,
-      "datasets" => dataset.reverse
-    }
+    @labels = labels
+    @dataset = dataset.reverse
+    @deck = deck.values.reverse
   end
 end
 
