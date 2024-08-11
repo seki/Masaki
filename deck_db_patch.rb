0f04e5869971c1bc147186a5cc07ef9afa083f7d
Masatoshi SEKI <m_seki@mva.biglobe.ne.jp>
2024-08-12 08:04:54 +0900

    patch

---
+++ deck_db_patch.rb
@@ -0,0 +1,15 @@
+require_relative 'src/world'
+
+$world = MasakiWorld.new
+
+ary = Masaki::Meta.db { |d|
+  d.execute("select deck from referer_all where date > '2024-01-01';").map {|x| x['deck']}
+}
+
+ary.each do |name|
+  unless $world.deck[name]
+    $world.add(name, true)
+    p name
+    sleep(rand * 0.5)
+  end
+end
